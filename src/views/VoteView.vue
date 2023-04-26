17c7abaed16dec97aba6d0158fc06b4d26db7719
shotam1415 <shotam1415@gmail.com>
2023-04-26 18:02:41 +0900

    gridレイアウト完了

--- src/views/VoteView.vue
+++ src/views/VoteView.vue
@@ -1,26 +1,38 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="344">
-      <v-img src="../assets/thumbnail dummy.jpg" height="200px"></v-img>
+    <v-container class="">
+      <v-row :justify="justifycontent.center">
+        <v-col cols="5" v-for="plan in plans" :key="plan">
+          <v-card class="mx-auto" max-width="344">
+            <v-img src="../assets/thumbnail dummy.jpg" height="200px"></v-img>
 
-      <v-card-title>タイトルダミー</v-card-title>
+            <v-card-title>タイトルダミー</v-card-title>
 
-      <v-card-subtitle
-        >説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー</v-card-subtitle
+            <v-card-subtitle
+              >説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー説明ダミー</v-card-subtitle
+            >
+
+            <v-btn block>投票する</v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="note">
+      <p>Cardのサムネイルに使用している画像の引用先</p>
+      著作者：<a
+        target="_blank"
+        href="https://jp.freepik.com/free-vector/flat-neon-gaming-youtube-thumbnail_33809384.htm#query=%E3%82%B5%E3%83%A0%E3%83%8D%E3%82%A4%E3%83%AB&position=49&from_view=keyword&track=sph"
+        >Freepik</a
       >
-
-      <v-btn block>投票する</v-btn>
-    </v-card>
-    <p>Cardのサムネイルに使用している画像の引用先</p>
-    著作者：<a
-      target="_blank"
-      href="https://jp.freepik.com/free-vector/flat-neon-gaming-youtube-thumbnail_33809384.htm#query=%E3%82%B5%E3%83%A0%E3%83%8D%E3%82%A4%E3%83%AB&position=49&from_view=keyword&track=sph"
-      >Freepik</a
-    >
+    </div>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.note {
+  text-align: center;
+}
+</style>
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
@@ -33,6 +45,17 @@ export default class VoteViewComponent extends Vue {
   mounted() {
     console.log(this.getdata());
   }
+
+  plans: object = [
+    { description: "Aの説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。", title: "A" },
+    { description: "Bの説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。", title: "B" },
+    { description: "Cの説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。", title: "C" },
+    { description: "Dの説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。説明です。", title: "D" },
+  ];
+
+  justifycontent: object = {
+    center: "center",
+  };
 
   async getdata() {
     const querySnapshot = await getDocs(collection(db, "plans"));
