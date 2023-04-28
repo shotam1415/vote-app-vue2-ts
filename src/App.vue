e071821a607a2cc98b14eed6a293df8d8a01fbb1
shotam1415 <shotam1415@gmail.com>
2023-04-28 15:31:41 +0900

    user情報をグローバル変数にした

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       </div>
 
       <v-spacer></v-spacer>
-
+      <v-btn text v-if="isCurrentUser">{{ isCurrentUser.name }}</v-btn>
       <v-btn v-if="auth.currentUser" @click="signOutEvent" elevation="2" icon><v-icon>mdi-logout</v-icon></v-btn>
     </v-app-bar>
     <v-main>
@@ -21,7 +21,6 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
-import "./firebase/firebase";
 import { getAuth, signOut } from "firebase/auth";
 import { getDoc, doc } from "@firebase/firestore";
 import db from "./firebase/firestore";
@@ -41,21 +40,30 @@ export default class AppComponent extends Vue {
       });
   }
   async getCurrentUser() {
-    const user_id = this.auth.currentUser?.uid;
+    const auth = getAuth();
+    const user_id = auth.currentUser?.uid;
     if (user_id) {
       const usersRef = doc(db, "users", user_id);
       const userSnap = await getDoc(usersRef);
       if (userSnap.exists()) {
         console.log("Document data:", userSnap.data());
+        this.$store.commit("setCurrentUser", userSnap.data());
       } else {
-        // doc.data() will be undefined in this case
         console.log("No such document!");
       }
     }
   }
 
-  mounted() {
-    this.getCurrentUser();
+  get isCurrentUser(): any {
+    if (this.$store.getters.currentUser) {
+      return this.$store.getters.currentUser;
+    }
+  }
+
+  async mounted() {
+    this.auth.onAuthStateChanged(() => {
+      this.getCurrentUser();
+    });
   }
 }
 </script>
