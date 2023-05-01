903cfe2e7298658811f77ff9f48232000448ab88
shotam1415 <shotam1415@gmail.com>
2023-05-01 16:16:23 +0900

    signin,signup、ユーザー判定ロジック調整

--- src/views/SignInView.vue.vue
+++ src/views/SignInView.vue.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="signin" v-if="!isAuth">
+  <div class="signin" v-if="!isCurrentUser">
     <v-card width="400px" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">ログイン</h1>
@@ -51,18 +51,19 @@ export default class SigninView extends Vue {
         console.log(errorCode);
       });
   }
-  get isAuth(): boolean | undefined {
-    if (this.$store.getters.isAuth) {
-      return this.$store.getters.isAuth;
+  get isCurrentUser(): any {
+    if (this.$store.getters.currentUser) {
+      return this.$store.getters.currentUser;
     }
   }
+
   async mounted() {
     //ユーザーの権限判定
     getAuth().onAuthStateChanged(() => {
-      if (!this.isAuth) {
+      if (!this.isCurrentUser) {
         return false;
       }
-      if (this.isAuth) {
+      if (this.isCurrentUser) {
         this.$router.push("/vote");
       }
     });
