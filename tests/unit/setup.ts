14865c5bf5f0a3af96b85e801dfba787cb940a42
shotam1415 <shotam1415@gmail.com>
2023-05-11 11:31:35 +0900

    プランのデータが格納されるとv-colが表示される

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import { config } from "@vue/test-utils";
 
 Vue.use(Vuetify);
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
+config.showDeprecationWarnings = false;
