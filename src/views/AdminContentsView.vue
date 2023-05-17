c224b4472b1b5ffda97eca37078e421ea022b906
shotam1415 <shotam1415@gmail.com>
2023-05-17 11:42:30 +0900

    contentsページを表示するロジック作成

---
+++ src/views/AdminContentsView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>contents</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class AdminContentsView extends Vue {}
+</script>
