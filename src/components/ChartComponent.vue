64627fed1248b31e83353ecbd37b36347b68248a
shotam1415 <shotam1415@gmail.com>
2023-05-25 18:39:11 +0900

    ChartComponent不要な記述を消した

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -4,12 +4,6 @@ import { Bar } from "vue-chartjs";
 export default {
   extends: Bar,
   props: ["chartData", "options"],
-  data() {
-    return {
-      // height: "800px",
-      // width: "800px",
-    };
-  },
   mounted() {
     this.renderChart(this.chartData, this.options);
   },
